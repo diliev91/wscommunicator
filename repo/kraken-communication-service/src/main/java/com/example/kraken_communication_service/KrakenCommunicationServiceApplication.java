8bc6ad94eb9835baee819f796b772ef1b54bdc87
Dimitar Iliev <diliev91@gmail.com>
2024-05-27 14:56:54 +0300

    initial commit

---
+++ repo/kraken-communication-service/src/main/java/com/example/kraken_communication_service/KrakenCommunicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kraken_communication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenCommunicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenCommunicationServiceApplication.class, args);
+	}
+
+}
---
+++ repo/kraken-communication-service/src/test/java/com/example/kraken_communication_service/KrakenCommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kraken_communication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakenCommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
